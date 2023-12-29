8b4e9045141b546e2680cb36a88a69880f0689be
anchen1011 <anchen1011@gmail.com>
2023-12-29 10:24:28 +0000

    update

--- build_memory_maze.py
+++ build_memory_maze.py
@@ -2,24 +2,32 @@ import jsonlines
 import sys
 sys.path.append('..')
 from tqdm import tqdm
+
+
+fname = 'samples_zhihu.jsonl'
+lang = 'Chinese'
+
 samples = []
-with jsonlines.open('samples_kiss.jsonl') as reader:
+with jsonlines.open(fname) as reader:
     for item in reader:
         samples.append(item)
 print(len(samples))
 
 from agents.memory_tree import MemoryTree
-with jsonlines.open('samples_kiss_pro.jsonl', mode='a') as writer:
+from utils.agent_utils import split_text_into_paragraphs
+from utils.file_utils import jsonl_filename_suffix
+with jsonlines.open(jsonl_filename_suffix(fname, 'pro'), mode='a') as writer:
+
     for sample in tqdm(samples[400:]):
         if 'passage' not in sample:
             continue
         passage = sample['passage']
-        sents = passage.split('\n')
+        sents = split_text_into_paragraphs(passage, max_length=200)
         sents = [sent.strip() for sent in sents if len(sent.strip()) > 0]
-        if len(sents) <= 80:
+        if len(sents) <= 8:
             continue
         try:
-            memory_maze = MemoryTree(lang='English')
+            memory_maze = MemoryTree(lang=lang)
             memory_maze.add_sents(sents)
             sample['memory_maze'] = memory_maze.tree
         except Exception as e:
