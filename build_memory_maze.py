164bf9eb7f2c6c48f0179a78b894958d218aa49c
anchen1011 <anchen1011@gmail.com>
2024-01-07 17:03:06 +0000

    data ppl improve

--- build_memory_maze.py
+++ build_memory_maze.py
@@ -4,8 +4,8 @@ sys.path.append('..')
 from tqdm import tqdm
 
 
-fname = 'samples_zhihu.jsonl'
-lang = 'Chinese'
+fname = 'samples_kiss.jsonl'
+lang = 'English'
 
 samples = []
 with jsonlines.open(fname) as reader:
@@ -18,11 +18,12 @@ from utils.lang_utils import split_text_into_paragraphs
 from utils.file_utils import jsonl_filename_suffix
 with jsonlines.open(jsonl_filename_suffix(fname, 'pro'), mode='a') as writer:
 
-    for sample in tqdm(samples[400:]):
+    for sample in tqdm(samples):
         if 'passage' not in sample:
             continue
         passage = sample['passage']
-        sents = split_text_into_paragraphs(passage, max_length=200)
+        block_length = 200 if lang == 'Chinese' else 940
+        sents = split_text_into_paragraphs(passage, max_length=block_length)
         sents = [sent.strip() for sent in sents if len(sent.strip()) > 0]
         if len(sents) <= 8:
             continue
